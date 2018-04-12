8af4ec9a3f2556ef6602e506922b73e6f379ebf6
zhangyufeng <zhangyufeng_v@didichuxing.com>
2018-04-12 11:17:37 +0800

    jiagou

--- src/test/java/com/example/didi/Test.java
+++ src/test/java/com/example/didi/Test.java
@@ -9,6 +9,24 @@ public class Test {
 
         System.out.println("分库分表：");
         System.out.println(Objects.hashCode(Long.valueOf("100100050898")%3));
+
+
+
+//        int[] a= {1,5,2,4,3};
+//        int temp=0;
+//        for(int i=a.length-1;i>0;i--){
+//            for(int j=0;j<i;j++){
+//                if(a[j]>a[j+1]){
+//                    temp=a[j];
+//                    a[j]=a[j+1];
+//                    a[j+1]=temp;
+//                }
+//            }
+//        }
+//        for(int m=0;m<a.length;m++)
+//            System.out.print(a[m]+"\t");
+
     }
 }
+
 
